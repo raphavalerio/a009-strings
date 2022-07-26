c7692726579d65242951a8c141958815c5a14911
Raphael Valerio <raphaelsvalerio@hotmail.com>
2022-07-25 22:23:23 -0300

    Exercicio A009 Resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,15 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+//Exercicio A
+console.log(minhaString.trim())
+
+//Exercicio B
+const comTrim = minhaString.trim()
+
+console.log(`Quantidade de caracteres:\n
+Com espeços: ${minhaString.length}\n
+Sem espaços: ${comTrim.length}`)
+
+//Exercicio C
+console.log(minhaString.replaceAll("________","sticioso"))
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,18 @@
-//Crie a const para a frase aqui
+//Exercicio A
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \n\"BOAS VINDAS, mas não deixe o gato sair.\""
+console.log(frase)
+
+//Exercicio B
+const novaFrase = frase.replaceAll('verde','rosa') && frase.replaceAll('azul','laranja')
+console.log(novaFrase)
+
+//Exercicio C
+console.log(`A nova string possui \"verde\"?\n 
+${novaFrase.includes("verde")}\n
+E \"laranja\"?\n
+${novaFrase.includes("laranja")}`)
+
+//Extra
+const msgMaiuscula = novaFrase.replaceAll("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR")
+console.log(msgMaiuscula)
+
